0a2bb290f359df3f3f8a1842667b988dea984a88
AlexeyKozlovski <leos1@mail.ru>
2019-12-06 22:42:10 +0300

    footer to work correctly
    footer to work correctly

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let foot = document.getElementById("footer");
+let details = document.getElementById("details");
 let fade = document.getElementById("fade");
+// let section = document.qu
 foot.addEventListener("click", changePosition);
 
 fade.classList.add("fade");		
 
 function changePosition() {	
-	if(foot.style.position == "static"){
+	if(foot.style.position == "static" && details.hasAttribute('open')){
 		foot.style.position = "sticky";
+		details.removeAttribute('open');
 		fade.classList.add("fade");
-
 	}else{
 		fade.classList.remove("fade");	
 		foot.style.position = "static";
-	}
-}
+		details.setAttribute('open', true);
 
+	}
+};
+
+// onclick="event.stopPropagation()"
