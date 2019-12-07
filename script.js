353a62728ee9c31351243e11384683a020080212
AlexeyKozlovski <leos1@mail.ru>
2019-12-07 19:09:44 +0300

    add cross block and do some response

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let foot = document.getElementById("footer");
 let details = document.getElementById("details");
 let fade = document.getElementById("fade");
-// let section = document.qu
-foot.addEventListener("click", changePosition);
 
+foot.addEventListener("click", changePosition);
 fade.classList.add("fade");		
 
 function changePosition() {	
@@ -15,8 +14,5 @@ function changePosition() {
 		fade.classList.remove("fade");	
 		foot.style.position = "static";
 		details.setAttribute('open', true);
-
 	}
 };
-
-// onclick="event.stopPropagation()"
